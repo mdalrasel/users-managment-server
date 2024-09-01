96982fc56cd4c0b84f5be0dee7e235a7ddd7d0a2
md al rasel <mdalrasel96@gmail.com>
2024-09-01 16:06:19 +0600

    newUser create

--- index.js
+++ index.js
@@ -3,22 +3,34 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+app.use(cors());
+app.use(express.json())
 
 const users = [
-    {id:1, name:'sabana', email:'sabana@gmail.com'},
-    {id:2, name:'sabnur', email:'sabnur@gmail.com'},
-    {id:3, name:'popy', email:'popy@gmail.com'},
-    {id:4, name:'mousumi', email:'mousumi@gmail.com'},
-    {id:5, name:'purnima', email:'purnima@gmail.com'}
+    { id: 1, name: 'sabana', email: 'sabana@gmail.com' },
+    { id: 2, name: 'sabnur', email: 'sabnur@gmail.com' },
+    { id: 3, name: 'popy', email: 'popy@gmail.com' },
+    { id: 4, name: 'mousumi', email: 'mousumi@gmail.com' },
+    { id: 5, name: 'purnima', email: 'purnima@gmail.com' }
 ]
 
-app.get('/users',(req,res)=>{
+app.get('/users', (req, res) => {
     res.send(users)
 })
-app.post('/users',(res,req)=>{
-    console.log(req.body)
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser)
+    console.log(req.body);
+    res.send(newUser)
 })
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
